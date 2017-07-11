e5bd111fbb31dbd540732f08f3ba3409c0ccbdd9
Amy-Kate Andrews <amy.kate.andrews@gmail.com>
2017-07-11 17:51:09 +0000

    settings

--- healthlift/settings.py
+++ healthlift/settings.py
@@ -152,12 +152,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    os.path.join(BASE_DIR, 'main/static/'),
-]
 
-STATIC_URL = '/main/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'main/static'),)
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
