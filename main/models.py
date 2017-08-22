76f50160d5b4c4a33c8c3f26ec32d0e932b2e06b
Amy-Kate Andrews <amy.kate.andrews@gmail.com>
2017-08-22 10:17:29 -0600

    fixing bio field

--- main/models.py
+++ main/models.py
@@ -58,7 +58,7 @@ class Employee(models.Model):
                             blank=True)
     title = models.CharField(max_length=150,
                              blank=True)
-    bio = models.TextField(max_length=350,
+    bio = RichTextField(max_length=350,
                            blank=True,
                            help_text="Brief bio of employee.")
     headshot = models.ImageField(upload_to="employees/",
